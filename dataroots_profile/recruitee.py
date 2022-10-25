06485cd7061fa71722f2fd5116d7c3249efc8586
Murilo Cunha <murilo@dataroots.io>
2022-10-25 21:00:38 +0200

    pedantry - code refac + linting

--- dataroots_profile/recruitee.py
+++ dataroots_profile/recruitee.py
@@ -1,20 +1,17 @@
 """Recruitee helpers for jobs dynamic info."""
 from dataclasses import dataclass
-from pprint import pprint
 from textwrap import dedent
-from typing import Any
 
 import requests
-
-URL_BASE = "https://careers.dataroots.io/o/"
 
 
 @dataclass
 class Offer:
-    """'Brief' offer information (instead of 'default') from Recruitee API."""
+    """Partial offer response from Recruitee API."""
 
     careers_url: int
     title: str
+    ...
 
 
 def jobs() -> list[Offer]:
@@ -23,19 +20,22 @@ def jobs() -> list[Offer]:
         "https://dataroots.recruitee.com/api/offers",
         headers={"accept": "application/json"},
     )
-    return [Offer(careers_url=offer["careers_url"], title=offer["title"]) for offer in response.json().get("offers", [])]
+    return [
+        Offer(careers_url=offer["careers_url"], title=offer["title"])
+        for offer in response.json().get("offers", [])
+    ]
 
 
-def offer2str(offer: Offer, *, url_base: str = URL_BASE) -> str:
+def offer2str(offer: Offer) -> str:
     """Get a formatted string for markdown profile from offer information."""
     return f"- [{offer.title}]({offer.careers_url})"
 
 
-def jobs2str(offers: list[Offer], **offer2str_kwargs: Any) -> str:
+def jobs2str(offers: list[Offer]) -> str:
     """Get a nice string with jobs from listing."""
     if not offers:
         raise ValueError(f"Expected list of offers, got {offers}.")
-    offers = "\n".join(offer2str(offer, **offer2str_kwargs) for offer in offers)
+    offers = "\n".join(offer2str(offer) for offer in offers)
     return dedent(
         f"""\
 ## Join our team! ❤️
@@ -48,7 +48,6 @@ For more info check out [dataroots.io/careers](https://dataroots.io/careers) �
     )
 
 
-def info(**offer2str_kwargs: Any) -> str:
+def info() -> str:
     """Get jobs information string for markdown profile."""
-    _jobs = jobs()
-    return jobs2str(_jobs, **offer2str_kwargs)
+    return jobs2str(jobs())
--- profile/README.md
+++ profile/README.md
@@ -52,10 +52,8 @@
 
 
 <!-- [[[cog
-import os
 import cog
 from dataroots_profile import recruitee
-
 
 cog.out(
     recruitee.info()
